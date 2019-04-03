6d23e25457ab1720296bdff5979caf603a91665e
Jerome <jerome@spinprotocol.io>
2019-04-04 00:07:30 +0900

    Write and Run Mutation in GraphQL Playground

--- server.js
+++ server.js
@@ -14,16 +14,28 @@ const typeDefs = gql`
   type Query {
     getTodos: [Todo]
   }
+
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;
+    }
   }
 };
 
 const server = new ApolloServer({
-  typeDefs, resolvers
+  typeDefs,
+  resolvers
 });
 
 server.listen().then(({ url }) => {
