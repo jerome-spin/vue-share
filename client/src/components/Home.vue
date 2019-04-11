4eee9511f80ea86ae62c59349adc49fc2942f2ae
Jerome <jerome@spinprotocol.io>
2019-04-11 10:39:48 +0900

    Use Queries with ApolloQuery Component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <div v-if='$apollo.loading'>Loading...</div>
-    <ul v-else v-for='post in getPosts' :key='post._id'>
-      <li>
-        {{ post.title }}
-        {{ post.imageUrl }}
-        {{ post.description }}
-      </li>
-      <li>{{ post.likes }}</li>
-    </ul>
+
+    <ApolloQuery :query='getPostsQuery'>
+      <template slot-scope='{ result: { loading, error, data, networkStatus } }'>
+        <div v-if='loading'>Loading...</div>
+        <div v-else-if='error'>Error! {{ error.message }}</div>
+        <div v-else-if='networkStatus'>Network Status: {{ networkStatus }}</div>
+        <ul v-else v-for='post in data.getPosts' :key='post._id'>
+          <li>
+            {{ post.title }}
+            {{ post.imageUrl }}
+            {{ post.likes }}
+          </li>
+        </ul>
+
+      </template>
+    </ApolloQuery>
+
   </v-container>
 </template>
 
@@ -20,12 +28,7 @@ export default {
   name: 'home',
   data() {
     return {
-      posts: []
-    }
-  },
-  apollo: {
-    getPosts: {
-      query: gql`
+      getPostsQuery: gql`
         query {
           getPosts {
             _id
@@ -36,13 +39,6 @@ export default {
           }
         }
       `,
-      result(args) {
-        console.dir(args);
-      },
-      error(err) {
-        console.error('[ERROR!!]', err);
-        console.dir(err);
-      }
     }
   }
 }
