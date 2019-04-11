70c7820b3102a65ef6c40dc9c669f206e2679f6f
Jerome <jerome@spinprotocol.io>
2019-04-11 23:41:45 +0900

    Add Carousel Component to Home Page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,23 +1,12 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-
-    <ApolloQuery :query='getPostsQuery'>
-      <template slot-scope='{ result: { loading, error, data, networkStatus } }'>
-        <div v-if='loading'>Loading...</div>
-        <div v-else-if='error'>Error! {{ error.message }}</div>
-        <div v-else-if='networkStatus'>Network Status: {{ networkStatus }}</div>
-        <ul v-else v-for='post in data.getPosts' :key='post._id'>
-          <li>
-            {{ post.title }}
-            {{ post.imageUrl }}
-            {{ post.likes }}
-          </li>
-        </ul>
-
-      </template>
-    </ApolloQuery>
-
+  <v-container text-xs-center v-if='getPosts'>
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval='3000'>
+        <v-carousel-item v-for='post in getPosts' :key='post._id' :src='post.imageUrl'>
+          <h1 id='carousel__title'>{{ post.title }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
@@ -26,9 +15,9 @@ import { gql } from 'apollo-boost'
 // @ is an alias to /src
 export default {
   name: 'home',
-  data() {
-    return {
-      getPostsQuery: gql`
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
           getPosts {
             _id
@@ -38,8 +27,22 @@ export default {
             likes
           }
         }
-      `,
+      `
     }
   }
 }
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0px;
+  right: 0px;
+}
+</style>
