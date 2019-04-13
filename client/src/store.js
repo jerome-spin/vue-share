99c6ae1f1ce1e2f3ce58366f689fb5900cea2dde
Jerome <jerome@spinprotocol.io>
2019-04-13 11:44:16 +0900

    Add Loading Property, Loading Spinner and mapGetters

--- client/src/store.js
+++ client/src/store.js
@@ -8,15 +8,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    posts: []
+    posts: [],
+    loading: false
   },
   mutations: {
     setPosts: (state, payload) => {
       state.posts = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload
     }
   },
   actions: {
     getPosts: ({ commit }) => {
+      commit('setLoading', true);
       // Use ApolloClient to fire getPosts query
       apolloClient
         .query({
@@ -34,14 +39,16 @@ export default new Vuex.Store({
           // Get data from actions to state via mutations
           // Commit passes data from actions along to mutation functions
           commit('setPosts', data.getPosts);
-          console.log(data.getPosts);
+          commit('setLoading', false);
         })
         .catch(err => {
+          commit('setLoading', false);
           console.err(err);
         });
     }
   },
   getters: {
-    posts: state => state.posts
+    posts: state => state.posts,
+    loading: state => state.loading
   }
 });
