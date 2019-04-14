474d8ddabb489f875d315c96bf0a35845ee302a2
Jerome <jerome@spinprotocol.io>
2019-04-14 16:32:09 +0900

    Create signinUser Mutation

--- resolvers.js
+++ resolvers.js
@@ -1,25 +1,29 @@
 /**
  *  Resolvers will be an Object.
- *  It will contain all the functionality for fetching data 
+ *  It will contain all the functionality for fetching data
  *  and for changing data with our database.
  */
+
+const bcrypt = require('bcrypt');
 
 module.exports = {
   Query: {
     getPosts: async (_, args, { Post }) => {
-      const posts = await Post.find({}).sort({ createdDate: 'desc' }).populate({
-        path: 'createdBy',
-        model: 'User'
-      });
+      const posts = await Post.find({})
+        .sort({ createdDate: 'desc' })
+        .populate({
+          path: 'createdBy',
+          model: 'User'
+        });
       return posts;
     }
   },
   Mutation: {
     addPost: async (
-        _,
-        { title, imageUrl, categories, description, creatorId },
-        { Post }
-      ) => {
+      _,
+      { title, imageUrl, categories, description, creatorId },
+      { Post }
+    ) => {
       const newPost = await new Post({
         title,
         imageUrl,
@@ -29,11 +33,18 @@ module.exports = {
       }).save();
       return newPost;
     },
-    signupUser: async (
-        _,
-        { username, email, password },
-        { User }
-      ) => {
+    signinUser: async (_, { username, password }, { User }) => {
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        throw new Error('Invalid password');
+      }
+      return user;
+    },
+    signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
       if (user) {
         throw new Error('User already exists');
@@ -46,4 +57,4 @@ module.exports = {
       return newUser;
     }
   }
-}
+};
