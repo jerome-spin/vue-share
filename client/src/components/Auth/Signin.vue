510175a298f1276fc1e9873c2bdd39851c4d3036
Jerome <jerome@spinprotocol.io>
2019-04-23 09:55:08 +0900

    Add Form Validation with Vuetify in Signin Component

--- client/src/components/Auth/Signin.vue
+++ client/src/components/Auth/Signin.vue
@@ -19,16 +19,28 @@
       <v-flex xs12 sm6 offset-sm3>
         <v-card color="secondary" dark>
           <v-container>
-            <v-form @submit.prevent="handleSigninUser">
+            <v-form
+              v-model="isFormValid"
+              lazy-validation
+              ref="form"
+              @submit.prevent="handleSigninUser"
+            >
               <v-layout row>
                 <v-flex xs12>
-                  <v-text-field v-model="username" prepend-icon="face" label="Username" type="text"></v-text-field>
+                  <v-text-field
+                    :rules="usernameRules"
+                    v-model="username"
+                    prepend-icon="face"
+                    label="Username"
+                    type="text"
+                  ></v-text-field>
                 </v-flex>
               </v-layout>
 
               <v-layout row>
                 <v-flex xs12>
                   <v-text-field
+                    :rules="passwordRules"
                     v-model="password"
                     prepend-icon="extension"
                     label="Password"
@@ -39,7 +51,7 @@
 
               <v-layout row>
                 <v-flex xs12>
-                  <v-btn :loading="loading" color="accent" type="submit">
+                  <v-btn :loading="loading" :disabled="!isFormValid" color="accent" type="submit">
                     <template v-slot:loader>
                       <span class="custom-loader">
                         <v-icon light>cached</v-icon>
@@ -68,8 +80,22 @@ export default {
   name: "Signin",
   data() {
     return {
+      isFormValid: true,
       username: "",
-      password: ""
+      password: "",
+      usernameRules: [
+        // Check if username in input
+        username => !!username || "Username is required",
+        // Make sure username is less than 10 characters
+        username =>
+          username.length < 10 || "Username must be less than 10 characters"
+      ],
+      passwordRules: [
+        password => !!password || "Password is required",
+        // Make sure password is at least 4 characters
+        password =>
+          password.length >= 4 || "Password must be more than 4 characters"
+      ]
     };
   },
   computed: {
@@ -85,10 +111,12 @@ export default {
   },
   methods: {
     handleSigninUser() {
-      this.$store.dispatch("signinUser", {
-        username: this.username,
-        password: this.password
-      });
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("signinUser", {
+          username: this.username,
+          password: this.password
+        });
+      }
     }
   }
 };
